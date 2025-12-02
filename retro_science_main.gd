b8c9ab9f342b77b439ce10fd6ae5b649223d903b
Jonathan <paralinguist@users.noreply.github.com>
2025-12-03 01:05:00 +0800

    Most functionality present

--- retro_science_main.gd
+++ retro_science_main.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var problem = ""
+var solution = ""
+var problem_type = "element"
+var special_available = false
+var failed_question = false
+
 var elements = ["hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", "nitrogen", "oxygen", "fluorine", "neon", "iridium"]
 var angles = ["right", "obtuse", "acute", "reflex", "straight", "full"]
 var planet_distances = {
@@ -14,12 +20,11 @@ var planet_distances = {
 }
 
 func generate_problem():
-	$"CanvasLayer/AI Screen".text = ""
-	var ai_font = $"CanvasLayer/AI Screen".get_theme_font("font")
-	var problem = ""
-	var solution = ""
-	var list_num = randi_range(1,1)
+	$"CanvasLayer/GamePanel/AI Screen".clear()
+	var ai_font = $"CanvasLayer/GamePanel/AI Screen".get_theme_font("font")
+	var list_num = randi_range(1,2)
 	if list_num == 1:
+		problem_type = "element"
 		var element = elements.pick_random()
 		problem = "How many examples of " + element + " are present?"
 		print(problem)
@@ -31,23 +36,121 @@ func generate_problem():
 				if pick == element:
 					occurances += 1
 				line += pick
-			while ai_font.get_string_size(line).x + 120 < $"CanvasLayer/AI Screen".size.x:
+			while ai_font.get_string_size(line).x + 120 < $"CanvasLayer/GamePanel/AI Screen".size.x:
 				line = line.insert(randi_range(3,len(line)-3), " ")
-			$"CanvasLayer/AI Screen".text += line + "\n"
-		$"CanvasLayer/AI Screen".text = $"CanvasLayer/AI Screen".text.left(-1)
+			$"CanvasLayer/GamePanel/AI Screen".text += line + "\n"
+		$"CanvasLayer/GamePanel/AI Screen".text = $"CanvasLayer/GamePanel/AI Screen".text.left(-1)
 		print("Occurances: " + str(occurances))
-		#Allow +/- 1 to be correct
-
+		solution = occurances
+	elif list_num == 2:
+		problem_type = "planets"
+		var probe_distance = snapped(randf_range(0.3, 30), 0.01)
+		print(probe_distance)
+		problem = "Sensors malfunctioning. Enter nearest planet to probe."
+		var closest_planet = "mercury"
+		var distance = 50
+		for planet in planet_distances:
+			if abs(planet_distances[planet] - probe_distance) < distance:
+				closest_planet = planet
+				distance = abs(planet_distances[planet] - probe_distance)
+		print(closest_planet)
+		solution = closest_planet
+		var probe_line = randi_range(0,8)
+		var probe_position = randi_range(0,3)
+		for i in range (9):
+			var line = ""
+			for j in range(4):
+				var planet_choice = planet_distances.keys().pick_random()
+				var pick =  planet_choice + " " + str(planet_distances[planet_choice])
+				if probe_line == i and probe_position == j:
+					pick = "probe " + str(probe_distance)
+				line += pick 
+			while ai_font.get_string_size(line).x + 120 < $"CanvasLayer/GamePanel/AI Screen".size.x:
+				line = line.insert(randi_range(3,len(line)-3), " ")
+			$"CanvasLayer/GamePanel/AI Screen".text += line + "\n"
+		$"CanvasLayer/GamePanel/AI Screen".text = $"CanvasLayer/GamePanel/AI Screen".text.left(-1)
+	$CanvasLayer/GamePanel/TextSubmission.placeholder_text = problem
+			
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _process(_delta: float) -> void:
+	if SpaceApi.connected:
+		$CanvasLayer/GamePanel.visible = true
+		if special_available:
+			$CanvasLayer/GamePanel/ButtonConsume.disabled = false
+			$CanvasLayer/GamePanel/ButtonEMP.disabled = false
+			$CanvasLayer/GamePanel/ButtonStartAI.disabled = true
+		else:
+			$CanvasLayer/GamePanel/ButtonConsume.disabled = true
+			$CanvasLayer/GamePanel/ButtonEMP.disabled = true
+			if not failed_question:
+				$CanvasLayer/GamePanel/Label.text = "The AI has a query for you..."
+				$CanvasLayer/GamePanel/ButtonStartAI.disabled = false
+		$CanvasLayer/ConnectPanel.visible = false
+	else:
+		$CanvasLayer/ConnectPanel.visible = true
+		$CanvasLayer/GamePanel.visible = false
 
 func _on_button_start_ai_pressed() -> void:
-	$CanvasLayer/ButtonStartAI.visible = false
+	$CanvasLayer/GamePanel/ButtonStartAI.visible = false
+	$CanvasLayer/GamePanel/TextSubmission.visible = true
+	$CanvasLayer/GamePanel/Label.text = "The AI wishes to know..."
 	generate_problem()
+
+
+func check_answer() -> void:
+	if problem_type == "element":
+		if !(int($CanvasLayer/GamePanel/TextSubmission.text) > solution + 1) and !(int($CanvasLayer/GamePanel/TextSubmission.text) < solution - 1):
+			failed_question = false
+		else:
+			failed_question = true
+	elif problem_type == "planets":
+		if $CanvasLayer/GamePanel/TextSubmission.text == solution:
+			failed_question = false
+		else:
+			failed_question = true
+	if failed_question:
+		$CanvasLayer/GamePanel/Label.text = "The AI is angry and upset"
+		$CanvasLayer/GamePanel/ButtonStartAI.disabled = true
+	else:
+		$CanvasLayer/GamePanel/Label.text = "This pleases the AI"
+		special_available = true
+	$CanvasLayer/GamePanel/ButtonStartAI.visible = true
+	$"CanvasLayer/GamePanel/AI Screen".clear()
+	$CanvasLayer/GamePanel/TextSubmission.clear()
+	$CanvasLayer/GamePanel/TextSubmission.visible = false
+	$TimerQuestionFinished.start()
+
+
+func _on_text_submission_text_changed() -> void:
+	if len($CanvasLayer/GamePanel/TextSubmission.text) > 1 and $CanvasLayer/GamePanel/TextSubmission.text[-1] == "\n":
+		$CanvasLayer/GamePanel/TextSubmission.text = $CanvasLayer/GamePanel/TextSubmission.text.trim_suffix("\n")
+		check_answer()
+
+
+func _on_button_connect_pressed() -> void:
+	SpaceApi.host = $CanvasLayer/ConnectPanel/TextServerIP.text
+	SpaceApi.server_connect(SpaceApi.host, SpaceApi.role, SpaceApi.team)
+
+func _on_button_shields_up_pressed() -> void:
+	SpaceApi.add_shield()
+
+
+func _on_button_consume_pressed() -> void:
+	SpaceApi.consume_shield()
+	special_available = false
+
+
+func _on_button_emp_pressed() -> void:
+	SpaceApi.emp()
+	special_available = false
+
+
+func _on_timer_question_finished_timeout() -> void:
+	$CanvasLayer/GamePanel/Label.text = "The AI is bored..."
+	failed_question = false
+	$CanvasLayer/GamePanel/ButtonStartAI.disabled = false
---
+++ space_api.gd
@@ -0,0 +1,123 @@
+extends Node
+
+var socket := WebSocketPeer.new()
+
+var host := "ws://localhost:9876"
+var port := "9876"
+var api_version = "0.93"
+var client_type = "godot"
+var role = "science"
+var team = "retro"
+
+var ship = { "total_power" : 4,
+		 "available_power" : 1,
+		 "pilot_power" : 1,
+		 "science_power" : 1,
+		 "weapon_power" : 1,
+		 "shield" : 0
+	}
+
+var connected := false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	socket.poll()
+
+	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
+		while socket.get_available_packet_count():
+			var server_message = socket.get_packet().get_string_from_ascii()
+			var server_response = JSON.new()
+			if server_response.parse(server_message) == OK:
+				var response = server_response.data
+				if response["type"] == "status":					
+					update_ship_status(response["data"])
+	else:
+		connected = false
+
+func update_ship_status(status):
+	ship["total_power"] = status["total_power"]
+	ship["available_power"] = status["available_power"]
+	ship["pilot_power"] = status["pilot_power"]
+	ship["science_power"] = status["science_power"]
+	ship["weapon_power"] = status["weapon_power"]
+	ship["shield"] = status["shield"]
+	ship["hp"] = status["hp"]
+					
+func log_message(message: String) -> void:
+	var time := "%s | " % Time.get_time_string_from_system()
+	print(time + message)
+
+func _exit_tree() -> void:
+	socket.close()
+
+func server_connect(_host, _role, _team):
+	host = _host
+	role = _role
+	team = _team
+	var websocket_url : String = "ws://"+host+":"+port
+	if socket.connect_to_url(websocket_url) != OK:
+		log_message("Unable to connect.")
+		set_process(false)
+	else:
+		var state = socket.get_ready_state()
+
+		while state == WebSocketPeer.STATE_CONNECTING:
+			state = socket.get_ready_state() 
+			socket.poll()
+		connected = true
+		var instruction = {"action":"join"}
+		send_instruction(instruction)
+		print("Connected!")
+		
+#Adds a single shield
+func add_shield() -> void:
+	var instruction = {"action":"shield"}
+	send_instruction(instruction)
+
+#Consumes a single shield
+func consume_shield() -> void:
+	var instruction = {"action":"consume"}
+	send_instruction(instruction)
+
+#Shoots selected module target weapon
+func shoot(target : String) -> void:
+	var instruction = {"action":"shoot", "weapon_id":target}
+	send_instruction(instruction)
+
+#Moves one space in direction
+func move(direction : String) -> void:
+	var instruction = {"action":"move", "direction":direction}
+	send_instruction(instruction)
+
+#Moves power up or down for a target module. Enforced by server.
+func power(direction, target):
+	var instruction = {"action":"power", "direction":direction, "target":target}
+	send_instruction(instruction)
+
+func overcharge():
+	var instruction = {"action":"overcharge"}
+	send_instruction(instruction)
+
+#Not applicable to the science module
+func special():
+	var instruction = {"action": "special"}
+	send_instruction(instruction)
+
+func craft():
+	var instruction  = {"action":"craft"}
+	send_instruction(instruction)
+
+func emp():
+	var instruction = {"action":"emp"}
+	send_instruction(instruction)
+
+func precognition():
+	var instruction = {"action":"precognition"}
+	send_instruction(instruction)
+
+func send_instruction(instruction: Dictionary):
+	#socket.put_packet(message.to_utf8_buffer())
+	instruction["role"] = role
+	instruction["team"] = team
+	instruction["version"] = api_version
+	socket.send_text(JSON.stringify(instruction))
